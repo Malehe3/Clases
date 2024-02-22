2536648ff2541d98edc1f0d15c7ee5094b9ea049
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-02-22 10:25:54 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -12,3 +12,15 @@ st.image(image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('Este es mi texto', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+coll, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+       st.write('Correcto!')
+        
