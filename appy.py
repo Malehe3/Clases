238b2180423c9e04400f545aeec832437e65c746
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-02-22 10:10:00 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -2,3 +2,9 @@ import streamlit as st
 from PIL import Image 
 
 st.title("Mi primera chamba!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png')
+
+st.image(image, caption='Interfaces multimodales')
