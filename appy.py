efad968a42f3de0ee17b1a7c8b441421dbbf8de3
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-02-22 10:16:41 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -11,4 +11,4 @@ st.image(image, caption='Interfaces multimodales')
 
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
-st.write('Es verdad que:', texto)
+st.write('Este es mi texto', texto)
